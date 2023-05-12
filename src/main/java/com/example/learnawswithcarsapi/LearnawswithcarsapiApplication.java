828ed5428bf2c771e44bca7acabb8e703c261cd8
Fanon Jupkwo <jupsfan@gmail.com>
2023-05-12 13:06:12 +0100

    project init

---
+++ src/main/java/com/example/learnawswithcarsapi/LearnawswithcarsapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnawswithcarsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnawswithcarsapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnawswithcarsapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnawswithcarsapi/LearnawswithcarsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnawswithcarsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnawswithcarsapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
